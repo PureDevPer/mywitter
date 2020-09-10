3c50d36231b552e8fe014f0aad9ac57d91326175
PureDevPer <wooseok.kim1207@gmail.com>
2020-09-10 02:23:14 -0400

    Router Setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3EPZfRtxqvDSghuTHBmKxoesPjITFuHc",
-  authDomain: "mywitter-ce310.firebaseapp.com",
-  databaseURL: "https://mywitter-ce310.firebaseio.com",
-  projectId: "mywitter-ce310",
-  storageBucket: "mywitter-ce310.appspot.com",
-  messagingSenderId: "859314881640",
-  appId: "1:859314881640:web:2f3b10da66f1b8f5a927dd",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
