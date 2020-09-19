e00509fa1af2467da5b7cefb2397f0790bda7cac
PureDevPer <wooseok.kim1207@gmail.com>
2020-09-19 00:57:18 -0400

    CRUD - Read

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async () => {
+    // https://firebase.google.com/docs/reference/js/firebase.firestore.CollectionReference#get
+    const dbTweets = await dbService.collection("myMessage").get();
+    dbTweets.forEach((document) => {
+      const tweetObj = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [tweetObj, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
+    // https://firebase.google.com/docs/reference/js/firebase.firestore.CollectionReference#add
     await dbService.collection("myMessage").add({
       tweet,
       createdAt: Date.now(),
@@ -17,6 +33,7 @@ const Home = () => {
     } = event;
     setTweet(value);
   };
+  console.log(tweets);
   return (
     <>
       <form onSubmit={onSubmit}>
@@ -29,6 +46,13 @@ const Home = () => {
         />
         <input type="submit" value="Tweet" />
       </form>
+      <>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </>
     </>
   );
 };
