053e4c072acdc9816ba9201c2058ba8a2827a3b2
PureDevPer <wooseok.kim1207@gmail.com>
2020-09-19 00:27:39 -0400

    Added form in Home and firebase/database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <>
+      <for onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={140}
+          value={tweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="Tweet" />
+      </for>
+    </>
+  );
+};
 export default Home;
