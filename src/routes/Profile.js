b3aa20045a75a6059f3a904196ca5743b859f954
PureDevPer <wooseok.kim1207@gmail.com>
2020-09-27 00:32:04 -0400

    Get my tweets on profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   // https://reactrouter.com/web/api/Hooks
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyTweet = async () => {
+    const tweets = await dbService
+      .collection("myMessage")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(tweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyTweet();
+  }, [getMyTweet]);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
