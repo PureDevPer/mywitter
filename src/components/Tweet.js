12142ec7fc74da48dc4b4ea80c7c2709c827502a
PureDevPer <wooseok.kim1207@gmail.com>
2020-09-27 00:14:00 -0400

    Delete Files as well as Text from storage and DB

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 
 const Tweet = ({ tweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
@@ -8,6 +8,8 @@ const Tweet = ({ tweetObj, isOwner }) => {
     const ok = window.confirm("Are you sure you want to delete this tweet?");
     if (ok) {
       await dbService.doc(`myMessage/${tweetObj.id}`).delete();
+      // https://firebase.google.com/docs/reference/js/firebase.storage.Storage#reffromurl
+      await storageService.refFromURL(tweetObj.attachmentUrl).delete();
     }
   };
   const toggleEditing = () => setEditing((prev) => !prev);
